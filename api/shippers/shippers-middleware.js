f36f084e01beb29fb143c617d1efbe220f526abe
Michaiah Bos <michaiahbos@yahoo.com>
2024-03-12 18:16:50 -0500

    wip

--- api/shippers/shippers-middleware.js
+++ api/shippers/shippers-middleware.js
@@ -1,12 +1,25 @@
-module.exports = {
-  checkId,
-  checkPayload,
-}
+const db = require('../../data/db-config')
 
-function checkId(req, res, next) {
-  next()
+async function checkId(req, res, next) {
+  const shipper = await db('shippers').where('shipperid', req.params.id).first()
+  if(!shipper){
+    next({status: 404, message: 'that id does not exist'})
+  } else {
+next();
+  }
+  
 }
 
 function checkPayload(req, res, next) {
-  next()
+  if (!req.body.Phone || !req.body.ShipperName){
+ next({ status: 422, message: 'Phone and ShipperName are required'})
+
+  } else {
+ next();
+  }
+
 }
+module.exports = {
+  checkId,
+  checkPayload,
+};
